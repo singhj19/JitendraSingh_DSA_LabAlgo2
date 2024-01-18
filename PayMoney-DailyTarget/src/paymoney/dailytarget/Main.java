e3e6125c5b15bf08a776b8981115e2fa8240aecf
singhj19 <145288309+singhj19@users.noreply.github.com>
2024-01-19 01:28:36 +0530

    Add files via upload
    PayMoney

---
+++ PayMoney-DailyTarget/src/paymoney/dailytarget/Main.java
@@ -0,0 +1,11 @@
+package paymoney.dailytarget;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DailyTargetDeterminator determinator = new DailyTargetDeterminator();
+        determinator.collectTransactionValues();
+        determinator.collectDailyTargets();
+        determinator.determine();
+    }
+}
